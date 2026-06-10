7473f98392348935f659e01e8e54f8bfe485aed1
VictoriaPortillo07 <portillov@udesa.edu.ar>
2026-06-09 21:06:36 -0300

    estructura clases

---
+++ clases.py
@@ -0,0 +1,26 @@
+class Ambiente:
+    def __init__(self,nombre,resistencia,attack,defense,speed):
+        self.nombre=
+        self.reistencia=
+        self.attack=
+        self.defense=
+        self.speed=
+
+class Evento_aleatorio:
+    def __init__(self,nombre,resistencia,attack,defense,speed):
+        self.nombre=
+        self.reistencia=
+        self.attack=
+        self.defense=
+        self.speed=
+    def modifica_ambiente(self,ambiente):
+        if self.nombre=='':
+            ambiente. -= 
+        if self.nombre=='':
+            ambiente. += 
+    
+    def post_evento(self,ambiente):
+        if self.nombre=='':
+            ambiente. += 
+        elif self.nombre=='':
+            ambiente. -= 
